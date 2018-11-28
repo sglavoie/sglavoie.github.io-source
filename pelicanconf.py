9de9f8b1ca1dd9d7e3c705da00d025525e623b97
Sébastien Lavoie <sgdlavoie@gmail.com>
2018-11-28 17:23:01 -0600

    Improve design of footer and add text to email link.

--- pelicanconf.py
+++ pelicanconf.py
@@ -6,7 +6,7 @@ from pelican import __version__
 
 PELICAN_VERSION = __version__
 
-SITE_VERSION = 'v0.3.3'
+SITE_VERSION = 'v0.3.4'
 CURRENT_YEAR = datetime.today().year
 
 AUTHOR = 'Sébastien Lavoie'
