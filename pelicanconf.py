d193fd57a624c9e196f7b6b51f8443a97668c30d
Sébastien Lavoie <sgdlavoie@gmail.com>
2018-12-10 18:57:47 -0600

    Set up menu items properly and RSS feeds.

--- pelicanconf.py
+++ pelicanconf.py
@@ -6,23 +6,34 @@ from pelican import __version__
 
 PELICAN_VERSION = __version__
 
-SITE_VERSION = 'v0.3.8'
+SITE_VERSION = 'v0.3.10'
 CURRENT_YEAR = datetime.today().year
 
 AUTHOR = 'Sébastien Lavoie'
 SITENAME = 'sglavoie.com'
-SITEURL = ''
+SITEURL = 'https://www.sglavoie.com'
 SITESUBTITLE = ''
 DISQUS_SITENAME = 'sglavoie'
 TWITTER_USERNAME = 'sgdlavoie'
 THEME = 'themes/SL'
 ABOUT_VERSION = '0.1.0'
 
+
 PATH = 'content'
 
 TIMEZONE = 'America/Mexico_City'
 
 DEFAULT_LANG = 'en'
+
+MENUITEMS = [
+    # path, id, icon
+    ('/index.html', 'home', '<i class="fas fa-home"></i>'),
+    ('/tags.html', 'tags', '<i class="fas fa-hashtag"></i>'),
+    ('/learning-process.html', 'learning', '<i class="fas fa-graduation-cap"></i>'),
+    ('/about.html', 'about', '<i class="fas fa-info"></i>')
+]
+
+
 
 # `fenced_code` enables the following syntax for code blocks and make it
 # possible to use special symbols, among other things:
@@ -40,11 +51,8 @@ MARKDOWN = {
 }
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
+FEED_ALL_RSS = 'feeds/all.rss.xml'
+CATEGORY_FEED_RSS = 'feeds/{slug}.rss.xml'
 
 # STATIC_PATHS = [
     # 'pictures',
@@ -61,10 +69,10 @@ DEFAULT_PAGINATION = 5
 USE_FOLDER_AS_CATEGORY = True
 DEFAULT_CATEGORY = 'misc'
 DISPLAY_CATEGORIES_ON_MENU = True
-TYPOGRIFY = False
+TYPOGRIFY = True
 ARTICLE_URL = 'posts/{date:%Y}/{date:%m}/{date:%d}/{slug}/'
 ARTICLE_SAVE_AS = 'posts/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
 DEFAULT_DATE_FORMAT = '%B %d, %Y'
 
 # Uncomment following line if you want document-relative URLs when developing
-# RELATIVE_URLS = True
+RELATIVE_URLS = False
