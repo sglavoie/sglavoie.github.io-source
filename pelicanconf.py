4915d5b524eb60961bd9a3e1778d48d6a70ce10b
Sébastien Lavoie <sgdlavoie@gmail.com>
2019-01-08 21:56:07 -0600

    Update website version to reflect changes in date format

--- pelicanconf.py
+++ pelicanconf.py
@@ -6,7 +6,7 @@ from pelican import __version__
 
 PELICAN_VERSION = __version__
 
-SITE_VERSION = 'v0.5.3'
+SITE_VERSION = 'v0.5.4'
 CURRENT_YEAR = datetime.today().year
 
 AUTHOR = 'Sébastien Lavoie'
