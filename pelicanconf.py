1c3162f9d8d7593fd1b62d6b4a71806320c27252
Sébastien Lavoie <sgdlavoie@gmail.com>
2019-02-18 22:40:23 -0600

    Add SITE_DESCRIPTION for meta Description

--- pelicanconf.py
+++ pelicanconf.py
@@ -7,14 +7,14 @@ from pelican import __version__
 PELICAN_VERSION = __version__
 
 ABOUT_VERSION = '0.1.2'
-SITE_VERSION = 'v0.10.4'
+SITE_VERSION = 'v0.10.5'
 CURRENT_YEAR = datetime.today().year
 
 AUTHOR = 'Sébastien Lavoie'
 SITENAME = 'sglavoie.com'
 SITEURL = 'https://www.sglavoie.com'
 SITESUBTITLE = 'The Learning Journey to the Summit of Commits'
-SITE_DESCRIPTION = ''
+SITE_DESCRIPTION = 'Provide useful information while documenting my journey as a learner in technology-related matters. Tech journal with the specific goal of explaining the challenges that I will inevitably face while trying to become a better student and most importantly, it will be a reference to the solutions that I found along the way so that it can benefit others too.'
 DISQUS_SITENAME = 'sglavoie'
 TWITTER_USERNAME = 'sgdlavoie'
 THEME = 'themes/SL'
