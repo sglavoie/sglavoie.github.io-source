8c80d61b0db422e4a159aae6f5b7a022b7238767
Sébastien Lavoie <sgdlavoie@gmail.com>
2019-03-06 18:10:09 -0600

    Add Stats section to About page (to improve)

--- pelicanconf.py
+++ pelicanconf.py
@@ -6,16 +6,23 @@ from pelican import __version__
 
 PELICAN_VERSION = __version__
 
-ABOUT_VERSION = '0.1.3'
+ABOUT_VERSION = '0.1.4'
 SITE_VERSION = 'v0.10.20'
 CURRENT_YEAR = datetime.today().year
 LAST_UPDATE = datetime.now()
+TIME_ZONE = 'CST'
 
 AUTHOR = 'Sébastien Lavoie'
 SITENAME = 'sglavoie.com'
 SITEURL = 'https://www.sglavoie.com'
 SITESUBTITLE = f'The Learning Journey to the Summit of Commits | {AUTHOR}'
-SITE_DESCRIPTION = 'Provide useful information while documenting my journey as a learner in technology-related matters. Tech journal with the specific goal of explaining the challenges that I will inevitably face while trying to become a better student and most importantly, it will be a reference to the solutions that I found along the way so that it can benefit others too.'
+SITE_DESCRIPTION = (
+        'Provide useful information while documenting my journey as a learner '
+        'in technology-related matters. Tech journal with the specific goal '
+        'of explaining the challenges that I will inevitably face while trying'
+        ' to become a better student and most importantly, it will be a '
+        'reference to the solutions that I found along the way so that it can '
+        'benefit others too.')
 DISQUS_SITENAME = 'sglavoie'
 TWITTER_USERNAME = 'sgdlavoie'
 THEME = 'themes/SL'
