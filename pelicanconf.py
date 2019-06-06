95e55f485915d3c85ebe9954497b143beac0f0a2
Sébastien Lavoie <sgdlavoie@gmail.com>
2019-06-06 13:52:44 -0500

    Add box-shadow effect to profile picture

--- pelicanconf.py
+++ pelicanconf.py
@@ -76,7 +76,7 @@ def get_cache_id(filename):
 
 PELICAN_VERSION = __version__
 
-ABOUT_VERSION = "0.7.0"
+ABOUT_VERSION = "0.8.0"  # major.minor.bug_fix
 SITE_VERSION = "v0.18.7"  # major.minor.bug_fix
 CURRENT_YEAR = datetime.today().year
 DEFAULT_DATE_FORMAT = "%B %d, %Y"
